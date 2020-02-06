5ba6131b27d8c3adabcefab77130332f9dbb2dc7
basar batur <baturbasar@gmail.com>
2020-02-06 17:12:08 -0600

    basar4

--- src/test/java/com/ortak/testbase/PageInitializer.java
+++ src/test/java/com/ortak/testbase/PageInitializer.java
@@ -23,7 +23,7 @@ public class PageInitializer extends BaseClass{
 	
 	public void print() {
 		System.out.println("Print smt");
-		System.out.println("Hatice change");
+		System.out.println("basar change");
 	}
 
 }
